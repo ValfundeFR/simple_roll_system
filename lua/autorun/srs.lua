47801158adb5409eec85874d59a0dcbe843ca4e8
Valfunde <valfunde@gmx.fr>
2017-08-11 18:25:58 +0200

    Update srs.lua

--- lua/autorun/srs.lua
+++ lua/autorun/srs.lua
@@ -1,3 +1,9 @@
+--[[-------------------------------------------------------------------------
+Script by Valfunde
+If you have any problems, don't hesitate to contact me or add me on steam (Valfunde *Banna Army) but write a comment before adding me
+If you try to edit something wich isn't in configuration file and the script doesn't work, it's you fault so don't come cry and tell script is broken.
+---------------------------------------------------------------------------]]
+
 include( "srs_config.lua" )
 local white, red = Color(255, 255, 255), Color(255, 50, 50)
 
