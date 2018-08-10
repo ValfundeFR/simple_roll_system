ed83f15e8be162af33528cd501fc641e4d570b54
Valfunde <valfunde@gmx.fr>
2018-08-11 00:30:43 +0200

    Update srs.lua

--- lua/autorun/srs.lua
+++ lua/autorun/srs.lua
@@ -4,18 +4,28 @@ If you have any problems, don't hesitate to contact me or add me on steam (Valfu
 If you try to edit something wich isn't in configuration file and the script doesn't work, it's you fault so don't come cry and tell script is broken.
 ---------------------------------------------------------------------------]]
 
-include( "srs_config.lua" )
+include("srs_config.lua")
+
 local white, red = Color(255, 255, 255), Color(255, 50, 50)
 
-hook.Add( "PlayerSay", "RollChatCommand", function( ply, text, public )
-	if string.lower( string.sub( text, 1, 5 ) ) == "/roll" then
-		local roll = math.random( 1, 100 )
-        for _, pl in pairs( player.GetAll( ) ) do 
-            if pl:GetPos( ):DistToSqr( ply:GetPos( ) ) < 100000 then
-                if CLIENT then return end
-                DarkRP.talkToPerson( pl, white, Valfunde.SRS.Roll .. " ", red, roll )
-            end
-        end
-        return ""
+hook.Add("PlayerSay", "RollChatCommand", function(ply, text, public)
+		
+	if string.lower(string.sub(text, 1, 5)) == "/roll" then
+
+		local roll = math.random(1, 100)
+			
+        	for _, player in ipairs(player.GetAll()) do
+				
+            		if player:GetPos():DistToSqr(ply:GetPos()) < 100000 then
+		
+				if CLIENT then return end
+					
+                		DarkRP.talkToPerson(player, white, Valfunde.SRS.Roll .. " ", red, roll)
+					
+            		end
+        	end
+			
+        	return ""
+
 	end
-end )
+end)
