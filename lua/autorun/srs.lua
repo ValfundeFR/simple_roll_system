74f54e95d90bc124b670996395417e3208d54864
Valfunde <valfunde@gmx.fr>
2018-08-11 00:31:19 +0200

    Update srs.lua

--- lua/autorun/srs.lua
+++ lua/autorun/srs.lua
@@ -1,6 +1,6 @@
 --[[-------------------------------------------------------------------------
 Script by Valfunde
-If you have any problems, don't hesitate to contact me or add me on steam (Valfunde *Banna Army) but write a comment before adding me
+If you have any problems, don't hesitate to contact me or add me on steam (Valfunde *Banana Army) but write a comment before adding me
 If you try to edit something wich isn't in configuration file and the script doesn't work, it's you fault so don't come cry and tell script is broken.
 ---------------------------------------------------------------------------]]
 
